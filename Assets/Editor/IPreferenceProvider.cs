caed3e9c45cd1ec91ad9a9761a0ff75ef4687ceb
Sonic853 <sonic853@qq.com>
2022-08-24 15:39:13 +0800

    add namespace

--- Assets/Editor/IPreferenceProvider.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections.Generic;
-
-public interface IPreferenceProvider
-{
-	void SetKeyValue(string valueName, object value);
-	void FetchKeyValues (IDictionary<string, object> prefsLookup);
-	object ValueField(string valueName, object value);
-}
---
+++ Assets/EditorPrefBrowser/Editor/IPreferenceProvider.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace EditorPref
+{
+    public interface IPreferenceProvider
+    {
+        void SetKeyValue(string valueName, object value);
+        void FetchKeyValues(IDictionary<string, object> prefsLookup);
+        object ValueField(string valueName, object value);
+    }
+}
